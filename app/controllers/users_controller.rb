8377c0e0fda4c661c96a5c514f998cb74ab333eb
nicmackenzie <nicmackenzie@live.com>
2023-08-19 10:09:51 +0300

    completed lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def render_not_found
+    render json: {error: "Not found"}, status: :not_found
   end
 
 end
